d442fd54f381c223fe943b175f6be5656da62938
Magne Davidsen <magne.davidsen@gmail.com>
2016-02-03 21:12:04 +0100

    using nest as thermostat

--- cron.js
+++ cron.js
@@ -3,6 +3,7 @@ var api = require('./api.js');
 var devices = require('./devices.js');
 var config = require('./config.js');
 var MemJS = require("memjs").Client
+var nest = require('nest-thermostat').init(process.env.nestUsername, process.env.nestPassword);
 
 var memjs = MemJS.create();
 
@@ -15,17 +16,20 @@ var api = new netatmo(config.netatmo);
 
 api.getStationsData(function(err, stations) {
   var actualTemp = stations[0].dashboard_data.Temperature;
-  memjs.get("thermostat", function(err, thermostat) {
-     if (actualTemp && thermostat) {
-       if(actualTemp > thermostat + 1) {
-          devices.getDevices().then(function(sensors){
-              sensors.filter(x => x.id === heatersGroupId).map(devices.turnOff);
+  nest.getInfo(process.env.nestSerial, function(data) {
+      var thermostat = data.target_temperature;
+      if (actualTemp && thermostat) {
+        if(actualTemp > thermostat + 1) {
+           devices.getDevices().then(function(sensors){
+               sensors.filter(x => x.id === heatersGroupId).map(devices.turnOff);
+            });
+         } else if(actualTemp < thermostat) {
+           devices.getDevices().then(function(sensors) {
+               sensors.filter(x => x.id === heatersGroupId).map(devices.turnOn);
            });
-        } else if(actualTemp < thermostat) {
-          devices.getDevices().then(function(sensors) {
-              sensors.filter(x => x.id === heatersGroupId).map(devices.turnOn);
-          });
+         }
         }
-       }
-     });
-   });
+  	console.log('Currently ' + data.current_temperature + ' degrees celcius');
+  	console.log('Target is ' + data.target_temperature + ' degrees celcius');
+  });
+ });
--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var netatmo = require('netatmo');
 var api = require('./api.js');
 var config = require('./config.js');
 var devices = require('./devices.js');
-var MemJS = require("memjs").Client
+var NestApi = require('nest-api');
+var nest = require('nest-thermostat').init(process.env.nestUsername, process.env.nestPassword);
 
-var memjs = MemJS.create();
 var app = express();
 
 const heatersGroupId = "1124573";
@@ -16,33 +16,14 @@ var devices = devices(telldusApi);
 var netatmoApi = new netatmo(config.netatmo);
 
 app.get('/', function (req, res) {
-  netatmoApi.getStationsData(function(err, stations) {
-    var actualTemp = stations[0].dashboard_data.Temperature;
-    memjs.get("thermostat", function(err, thermostat) {
-      devices.getDevices().then(function(sensors){
-
-          if (thermostat) {res.send("Heat on: " + sensors.filter(x => x.id === heatersGroupId)[0].state + ", Temperature: " + actualTemp + ", Thermostat: " + thermostat); }
-          else { res.send("Temperature: " + actualTemp + ", Thermostat: No value "); }
-
-       });
-
-       });
-     });
-   });
-
-app.get('/settemp/:thermostat', function (req, res) {
-  var responseText = "";
-  memjs.get("thermostat", function(err, thermostat) {
-    if(req.params.thermostat && !isNaN(req.params.thermostat)){
-      responseText = "Thermostat was changed from " + thermostat + " to " + req.params.thermostat;
-      memjs.set("thermostat", req.params.thermostat);
-  } else {
-      responseText = "Thermostat was not changed, still on " + thermostat;
-  }
-    res.send(responseText);
+    netatmoApi.getStationsData(function(err, stations) {
+        var actualTemp = stations[0].dashboard_data.Temperature;
+        nest.getInfo(process.env.nestSerial, function(data) {
+            var thermostat = data.target_temperature;
+            res.send("Temperature: " + actualTemp + ", Thermostat: " + thermostat);
+        });
+    });
 });
-});
-
 
 app.listen(process.env.PORT, function () {
   console.log('Example app listening on port ' + process.env.PORT);
